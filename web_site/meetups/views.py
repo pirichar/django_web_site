8912df2666340881638b9070730fa9c31d2c7d2b
Pier-Luc Richard <pier-lucrichard@Pier-Lucs-MacBook-Air.local>
2022-04-06 08:51:11 -0400

    first commit

---
+++ web_site/meetups/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	meetups = [
+		{ 
+			'title': 'A first Meetup', 
+			'location': 'New York', 
+			'slug': 'a-first-meetup' 
+		},
+		{ 
+			'title': 'A second Meetup',
+			'location': 'PARIS',
+			'slug': 'a-second-meetup' 
+		}
+	]
+	return render(request, 'meetups/index.html', {
+		'show_meetups': True,
+		'meetups': meetups
+	})
+
+def meetup_details(request, meetup_slug):
+	print(meetup_slug)
+	selected_meetup = {
+		'title': 'A First Meetup',
+		'description': 'This is the first meetup'
+		}
+	return render(request, 'meetups/meetup-details.html', {
+		'meetup_title': selected_meetup['title'],
+		'meetup_description': selected_meetup['description']
+	})
