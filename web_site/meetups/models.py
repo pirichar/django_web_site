fd498676a09e2d1264c26d5e8ef379f71f89ee28
WSL PL <plrichard@me.com>
2022-04-10 14:12:30 -0400

    just started with sqlite3

--- web_site/meetups/models.py
+++ web_site/meetups/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# all the custom models need to unherit of the models.model class
+# think at the model instances as object you will be working with in your code
+
+# Django will take your define models and will automatically create 
+# data base tables in that sqlite database out of the box
+# and will createa one table per model
+
+class Meetup(models.Model):
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(unique=True)
+	description = models.TextField()
